273ddce04b3e9d2a7eb706358793788aa627f40e
OlabisiO <oolabisiogundairo@gmail.com>
2020-01-15 17:02:23 +0100

    first web commit

---
+++ olabisi/olabisiapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
+
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'olabisiapp/post_list.html', {'what': posts})
+
+
+def post_detail(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'olabisiapp/post_detail.html', {'whom': posts})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'olabisiapp/post_edit.html', {'Myform': form})
